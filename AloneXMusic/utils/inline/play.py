6156b4806e9bc7cc59200b0ffabdae468fc24adb
˹ʏᴜᴍɪ ꭙ˼ <146187449+YumiXBot@users.noreply.github.com>
2023-12-17 22:59:49 +0530

    Update play.py

--- AloneXMusic/utils/inline/play.py
+++ AloneXMusic/utils/inline/play.py
@@ -66,22 +66,6 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-                 [
-            InlineKeyboardButton(
-
-                text="ᴅᴇᴠ 💕",
-
-                url=f"t.me/{OWNER_USERNAME}",
-
-            ),
-            InlineKeyboardButton(
-
-                text="💌 ɢʀᴏᴜᴘ",
-
-                url=f"{SUPPORT_CHAT}",
-
-            ),
-        ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
@@ -95,22 +79,6 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-                 [
-            InlineKeyboardButton(
-
-                text="ᴅᴇᴠ 💕",
-
-                url=f"t.me/{OWNER_USERNAME}",
-
-            ),
-            InlineKeyboardButton(
-
-                text="💌 ɢʀᴏᴜᴘ",
-
-                url=f"{SUPPORT_CHAT}",
-
-            ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
